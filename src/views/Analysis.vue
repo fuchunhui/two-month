d43c6040248a5686ff57e1ba2116fc1f70a5db07
fuchunhui <fuchuhu7@gmail.com>
2020-11-02 16:10:14 +0800

    wip: setup props

--- src/views/Analysis.vue
+++ src/views/Analysis.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="analysis">
-    <condition v-model:info="info"></condition>
+    <condition v-model:info="info" @aa="click" v-model:dept="dept"></condition>
     <container></container>
   </div>
 </template>
@@ -9,6 +9,7 @@
 import Condition from './analysis/Condition.vue';
 import Container from './analysis/Container.vue';
 import {BANK} from '@/config/index';
+import { reactive, toRef, toRefs, ref, Ref } from 'vue';
 
 export default {
   name: 'Analysis',
@@ -18,19 +19,50 @@ export default {
     Container
   },
 
+  data() {
+    return {
+      info1: {
+        year: 2020,
+        month: 9,
+        card: '0797'
+      }
+    };
+  },
+
   setup() {
     const date: Date = new Date();
     const year: number = date.getFullYear();
     const month: number = date.getMonth() + 1;
     const card: string = BANK.num1;
-    const info: object = {
+    const test: object = {
       year,
       month,
       card
     };
+    const dept: Ref<string> = ref('企业智能平台部');
+    console.log('iiii: ', reactive(test));
+    console.log('dept: ', dept);
+    const newTest = toRefs({
+      test
+    });
+    console.log('newTest: ', newTest);
+    const reactiveTest = reactive({
+      test
+    });
+    console.log('reactiveTest: ', reactiveTest);
     return {
-      info
+      info: newTest.test,
+      dept
     };
+  },
+
+  methods: {
+    click(value: string) {
+      console.log(value, '------aaa-------');
+    },
+    test(value: object) {
+      console.log(value, '-------test------');
+    }
   }
 };
 </script>
--- src/views/analysis/Condition.vue
+++ src/views/analysis/Condition.vue
@@ -5,7 +5,7 @@
         v-for="item in bankList"
         :key="item.value"
         :text="item.value"
-        :active="item.value === currentCard"
+        :active="item.value === info.card"
         @click="change(item.value, 'currentCard')">
       </card>
     </div>
@@ -14,7 +14,7 @@
         v-for="item in monthList"
         :key="item.value"
         :text="item.label"
-        :active="item.checked"
+        :active="item.value === info.month"
         @click="change(item.value, 'currentMonth')">
       </card>
     </div>
@@ -23,16 +23,17 @@
         v-for="item in yearList"
         :key="item.value"
         :text="item.label"
-        :active="item.checked"
+        :active="item.value === info.year"
         @click="change(item.value, 'currentYear')">
       </card>
     </div>
-    {{ currentCard }} - {{ current.card }}
+    <!-- {{ currentCard }} - {{ current.card }} -->
+    {{ dept }}
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, reactive, ref, toRefs} from 'vue';
+import {defineComponent, ref, toRefs, watch} from 'vue';
 import Card from './Card.vue';
 import {BANK, MONTH, YEAR} from '@/config/index';
 
@@ -54,11 +55,16 @@ export default defineComponent({
           card: '0797'
         };
       }
+    },
+    dept: {
+      type: String,
+      required: true,
+      default: '开开嘻嘻部'
     }
   },
 
-  setup(props, context) {
-    console.log('setup: ', {...props.info}, context);
+  setup(props, { emit }) {
+    console.log('setup: ', {...props.info}, emit);
     // TODO 处理事件，选中逻辑，高亮 + 切换判断 + 禁用选择
     // 卡片必选一个
     // 月份可以多选，也可不选择，不选择，默认全年数据
@@ -66,19 +72,21 @@ export default defineComponent({
     // const {year, month, card} = toRefs(props.info);
     // 思路A-2
     const {info} = toRefs(props);
-    console.log('init info: ', info);
-    const currentCard = ref(info.value.card);
+    // console.log('init info: ', info);
+    // const currentCard = ref(info.value.card);
 
     // 思路B
-    const current = ref(info.value);
-    console.log('current', current);
+    // const current = ref(info.value);
+    // console.log('current', current);
     // const {year, month, card} = props.info;
 
     // 此种当然没问题呀 思路A-1
     // const {year, month, card} = props.info; // eslint-disable-line
     // const currentCard = ref(card);
     // console.log('currentCard: ', card, currentCard);
-
+    watch(info, (nv, ov) => {
+      console.log(nv, ov);
+    });
     const bankList = [BANK.num1, BANK.num2].map(item => {
       return {
         label: item,
@@ -101,18 +109,32 @@ export default defineComponent({
     function change(value: string | number, key: string) {
       console.log(value, key);
       if (key === 'currentCard') {
+        emit('update:info', {
+          year: 2020,
+          month: 1,
+          card: value
+        });
+
+        emit('aa', {
+          year: 2020,
+          month: 1,
+          card: value
+        });
+
+        emit('update:dept', '伤心总是难免的部');
         // 思路A
         // currentCard.value = value as string;
         // console.log('change: ', currentCard.value);
 
         // 思路B
-        current.value.card = value;
-        console.log('change: ', current.value);
+        // current.value.card = value;
+        // info.value.card = value;
+        // console.log('change: ', current.value);
       }
     }
     return {
-      currentCard,
-      current,
+      // currentCard,
+      // current,
       change,
       bankList,
       monthList,
