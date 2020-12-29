57c5fa760cc99fffdd5b06f834a7cae99008e003
fuchunhui <fuchuhu7@gmail.com>
2020-12-29 11:50:41 +0800

    feat: add symbol

---
+++ src/components/road/again/operator.ts
@@ -0,0 +1,15 @@
+let s1 = '2';
+let s2 = 'z';
+let b = false;
+let f = 1.1;
+let o = {
+    valueOf() {
+        return -1;
+    }
+};
+
+console.log(s1++);
+console.log(s2++);
+console.log(b++);
+console.log(f--, f);
+console.log(o--, o);
