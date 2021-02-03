0e1cfe2c5f797b476bb7477b40aac5e4a29f4240
fuchunhui <fuchuhu7@gmail.com>
2021-02-03 14:23:22 +0800

    feat: add line-gradient

--- src/views/episodes/MagicCSS.vue
+++ src/views/episodes/MagicCSS.vue
@@ -4,8 +4,12 @@
       <div class="magic-wrap-item first-item">
         <div class="hover-first-item" />
       </div>
-      <div class="magic-wrap-item" />
-      <div class="magic-wrap-item" />
+      <div class="magic-wrap-item second-item">
+        <time datetime="2021-02-1" pubdate="">February 1st, 2021</time>
+      </div>
+      <div class="magic-wrap-item third-item">
+        123
+      </div>
       <div class="magic-wrap-item" />
       <div class="magic-wrap-item" />
       <div class="magic-wrap-item" />
@@ -23,6 +27,9 @@ export default {
 </script>
 
 <style lang="less">
+:root {
+  --magicBorderColor: #03A9F3;
+}
 @borderColor: #03A9F3;
 .magic-css {
   display: grid;
@@ -38,6 +45,10 @@ export default {
     border: 1px solid #222;
     border-radius: 0.5rem;
     padding: 2rem;
+
+    time::before {
+      content: '🌟 ';
+    }
   }
   .first-item {
     grid-column: 1/0;
@@ -61,7 +72,8 @@ export default {
       top: -10px;
       left: -10px;
       border-top: 1px solid @borderColor;
-      border-left: 1px solid @borderColor;
+      // border-left: 1px solid @borderColor;
+      border-left: 1px solid var(--magicBorderColor);
     }
     &::after {
       right: -10px;
@@ -75,6 +87,12 @@ export default {
       width: calc(100% + 19px);
       height: calc(100% + 19px);
     }
+  }
+  .second-item {
+    background-image: linear-gradient(45deg, rgb(55, 59, 68), rgb(66, 134, 244));
+  }
+  .third-item {
+    background: #03A9F3;
   }
 }
 </style>
