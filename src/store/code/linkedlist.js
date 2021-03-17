30d8a9d7012eafbca11d3f9519853b85241a817d
fuchunhui <fuchuhu7@gmail.com>
2021-03-17 11:20:14 +0800

    feat: add remove and findPrev

--- src/store/code/linkedlist.js
+++ src/store/code/linkedlist.js
@@ -11,8 +11,8 @@ function LList() {
   this.head = new Node('head');
   this.find = find;
   this.insert = insert;
-  // this.remove = remove;
-  // this.findPrev = findPrev;
+  this.remove = remove;
+  this.findPrev = findPrev;
   this.display = display;
 }
 
@@ -33,12 +33,28 @@ function insert(newElement, item) {
   currNode.next = newNode;
 }
 
+// 显示链表元素
 function display() {
   let currNode = this.head;
-  while (currNode.next !== null) {
+  while (currNode && currNode.next !== null) {
     console.log(currNode.next.element);
     currNode = currNode.next;
   }
+}
+
+function findPrev(item) {
+  let currNode = this.head;
+  while (currNode.next !== null && currNode.next.element !== item) {
+    currNode = currNode.next;
+  }
+  return currNode;
+}
+
+function remove(item) {
+  let prevNode = this.findPrev(item);
+  console.log(`移除的item：${item}, 找到的prevNode为${prevNode.element}`);
+  prevNode.next = prevNode.next.next;
+  item.next = null; // 是否删除item，仅仅置空就可以吗？
 }
 
 const fruits = new LList();
@@ -47,6 +63,14 @@ fruits.insert('banana', 'apple');
 fruits.insert('orange', 'banana');
 fruits.insert('peach', 'banana');
 fruits.insert('group', 'apple');
-
-console.log(fruits.display());
+fruits.insert('strawberry', 'group');
+fruits.insert('litchi', 'banana');
+fruits.display();
+const findElement = 'orange';
+const prevNode = fruits.findPrev(findElement);
+console.log(`${findElement}的prevNode是：`, {...prevNode});
+console.log('分界线----------', prevNode.element);
+fruits.remove(prevNode.element);
+fruits.remove('apple');
+fruits.display();
 
