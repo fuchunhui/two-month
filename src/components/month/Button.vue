4525d18c4f71e5179e0cdb84d5d9da2b3d27d618
fuchunhui <fuchuhu7@gmail.com>
2021-06-04 18:14:09 +0800

    wip: acquisition button

--- src/components/month/Button.vue
+++ src/components/month/Button.vue
@@ -30,5 +30,54 @@ export default defineComponent({
 .month-button {
   min-width: 98px;
   height: 32px;
+  background-color: #FFFFFF;
+  border: 1px solid #D9D9D9;
+  border-radius: 4px;
+  font-size: 12px;
+  cursor: pointer;
+  user-select: none;
+  vertical-align: middle;
+
+  &:hover {
+    border-color: #4B98F8;
+    color: #4B98F8;
+  }
+
+  &.disabled {
+    &,
+    &:hover {
+      border-color: #DDDEE4;
+      color: rgba(102, 102, 102, 0.4);
+      opacity: 1;
+    }
+  }
+
+  &[u~="primary"] {
+    background-color: #4B98F8;
+    border-color: transparent;
+    color: #FFFFFF;
+    &:hover {
+      background-color: #388AFA;
+      border-color: #388AFA;
+    }
+  }
+
+  &[u~="grey"] {
+    background-color: #F6F6F6;
+    color: #666666;
+
+    &:hover {
+      background-color: #F0F0F0;
+    }
+    &:active {
+      background-color: #ECECEC;
+    }
+
+    &:disabled,
+    &:disabled:hover {
+      background-color: #F6F6F6;
+      opacity: .4;
+    }
+  }
 }
 </style>
--- src/views/Acquisition.vue
+++ src/views/Acquisition.vue
@@ -3,19 +3,18 @@
     <div class="acquisition-record" v-if="showRecord">
       <textarea>这里是录入内容框</textarea>
     </div>
-    <!-- <div class="acquisition-content" v-else> -->
-    <div class="acquisition-content">
+    <div class="acquisition-content" v-else>
       <div>左侧表格</div>
       <div>右侧列表</div>
     </div>
     <div class="acquisition-btn">
       <month-button label="录入"/>
-      <month-button label="分析"/>
-      <month-button label="入库"/>
-      <month-button :label="emptyBtn"/>
-      <button @click="handleLabel">
+      <month-button label="分析" u="primary"/>
+      <month-button label="入库" u="primary"/>
+      <month-button label="清空" u="grey"/>
+      <!-- <button @click="handleLabel">
         测试按钮
-      </button>
+      </button> -->
     </div>
   </div>
 </template>
@@ -47,10 +46,27 @@ export default defineComponent({
 </script>
 
 <style lang="less">
+@import url('css/mixins.less');
 .acquisition {
+  display: flex;
+  flex-direction: column;
   padding: 10px;
+  &-record,
+  &-content {
+    flex: 1;
+    min-height: 300px;
+    background-color: cadetblue;
+  }
   &-btn {
-    background-color: #FF0000;
+    .flex-center();
+    height: 60px;
+    border-top: 1px solid #DDD;
+    .month-button {
+      margin-right: 20px;
+    }
+    .month-button:last-child {
+      margin-right: 0;
+    }
   }
 }
 </style>
